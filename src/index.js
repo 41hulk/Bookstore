e5f60d1427971f3d8b9df0edbfaa05f4f609492b
Ntare Guy <gntare2@gmail.com>
2022-03-29 15:05:15 +0200

    add store to index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './redux/configureStore';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
