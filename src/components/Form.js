605f2200c164ff1bdf32db6f8ff2731c54a5dc9e
Ntare Guy <gntare2@gmail.com>
2022-03-29 17:06:00 +0200

    add event-listner on button

--- src/components/Form.js
+++ src/components/Form.js
@@ -33,11 +33,11 @@ const Form = () => {
   };
 
   return (
-    <form method="POST" action="/" className="add" onSubmit={(e) => submitForm(e)}>
+    <form method="POST" action="/" className="add">
       <h3>ADD NEW BOOK</h3>
       <input type="text" name="title" id="title" value={book.title} placeholder="Book title" onChange={handleChange} required />
       <input type="text" name="author" id="author" value={book.author} placeholder="Author" onChange={handleChange} required />
-      <button type="submit">Add Book</button>
+      <button type="submit" onClick={(e) => submitForm(e)}>Add Book</button>
     </form>
   );
 };
